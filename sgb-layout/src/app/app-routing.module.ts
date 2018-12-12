4307c19985a6229e457f71cf7707f3d712db8cbe
Leonardo Dutra <leonardo.dutra@crateus.ufc.br>
2018-12-12 12:01:47 -0300

    #2 - Criação da dashboard e iniciando composição da página

--- sgb-layout/src/app/app-routing.module.ts
+++ sgb-layout/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard', 
+    component: DashboardComponent,
+    data: {
+      title: "Sistema de Gestão de Benefícios"
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sgb-layout/src/app/app.module.ts
+++ sgb-layout/src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCommonModule} from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
+  ],
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

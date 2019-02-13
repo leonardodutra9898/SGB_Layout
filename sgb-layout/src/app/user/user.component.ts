b811037ba916de53d508044ca92e3e7fc16ba165
Leonardo (NPDS) <leonardo.dutra@crateus.ufc.br>
2019-02-13 14:40:43 -0200

    #5 - inserindo modal para botoes de exclusao

--- sgb-layout/src/app/user/user.component.ts
+++ sgb-layout/src/app/user/user.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
+
+
 export class UserComponent implements OnInit {
 
   constructor() { }
